61438b485d11b1ceaeae3e81dd2e8b67002bbe4d
Thara Supasiti <sapiensis@gmail.com>
2023-09-18 10:18:27 +1000

    refactor lsp config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -165,22 +165,25 @@ lsp_config.tsserver.setup({
     }
 })
 
+local format_py = function(ev)
+    -- Use autopep8 command to change the file and then tell nvim to reload
+    local command = { "autopep8", "--in-place", ev.match, "--max-line-length", "120" }
+    vim.fn.jobstart(command, {
+        stdout_buffered = true,
+        on_stdout = function()
+            -- Use :checktime to sync buffer with the file that is just written by autopep8
+            vim.cmd("checktime")
+            print("formatted")
+        end
+    })
+end
+
 lsp_config.pyright.setup({
     -- This is for auto formatting without null-ls
     on_attach = function(_, bufnr)
         vim.api.nvim_create_autocmd("BufWritePost", {
             buffer = bufnr,
-            callback = function(ev)
-                -- Use autopep8 command to change the file and then tell nvim to reload
-                local command = { "autopep8", "--in-place", ev.match, "--max-line-length", "120" }
-                vim.fn.jobstart(command, {
-                    stdout_buffered = true,
-                    on_stdout = function()
-                        vim.cmd("checktime")
-                        print("formatted")
-                    end
-                })
-            end
+            callback = format_py
         })
     end,
     capabilities = capabilities,
@@ -191,9 +194,6 @@ lsp.format_on_save({
     servers = {
         ['rust_analyzer'] = { 'rust' },
         ['gopls'] = { 'go' },
-        -- if you have a working setup with null-ls
-        -- you can specify filetypes it can format.
-        -- ['null-ls'] = {'javascript', 'typescript'},
     }
 })
 
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,10 +1,10 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>ps', function()
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fs', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 
 -- Allow to search in folder paths
-vim.keymap.set('n', '<leader>pg', require("telescope").extensions.live_grep_args.live_grep_args, { noremap = true })
+vim.keymap.set('n', '<leader>fg', require("telescope").extensions.live_grep_args.live_grep_args, { noremap = true })
